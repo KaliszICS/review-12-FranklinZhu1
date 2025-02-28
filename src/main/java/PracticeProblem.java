9822052a6a920618f3a8e8e7956ac9105e634bbc
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-02-28 14:50:23 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,39 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static boolean isPalindrome(String word) {
+		word = word.toLowerCase().strip();
+		if (word.equals("")) return true;
+		int i = 0;
+		int j = word.length() - 1;
+		while (true) {
+			while (word.charAt(i) == ' ') ++i;
+			while (word.charAt(j) == ' ') --j;
+			if (word.charAt(i) != word.charAt(j)) return false;
+			if (j - i < 2) return true;
+			++i;
+			--j;
+		}
 	}
+
+	public static int getAge(String[] names, int[] ages, String name) {
+		for (int i = 0; i < names.length; ++i) {
+			if (names[i].equals(name)) return ages[i];
+		}
+		return -1;
+	}
+
+	public static int countWords(String s, char c) {
+		s = s.strip();
+		int count = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			if (s.charAt(i) == c) {
+				++count;
+				while (i < s.length() && s.charAt(i) != ' ') ++i; // jump to next whitespace
+				while (i < s.length() && s.charAt(i) == ' ') ++i; // jump to next word
+				--i; // to account for the for loop increment
+			}
+		}
+		return count;
+	}
+
 }
